ae1090ba2bcbfe752b5b3c7b759d32f0bba4096d
Edward <damon_edward@qq.com>
2023-11-07 20:14:20 +0800

    feat: nacos 元数据添加

---
+++ vita-gateway/src/main/java/com/vita/gateway/config/NacosConfiguration.java
@@ -0,0 +1,36 @@
+package com.vita.gateway.config;
+
+import com.alibaba.cloud.nacos.NacosDiscoveryProperties;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import javax.annotation.Resource;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
+@Configuration
+public class NacosConfiguration {
+
+    /**
+     * app 版本号
+     */
+    private static final String APP_VERSION = "app.version";
+
+    @Resource
+    private Environment env;
+
+    @Bean
+    @ConditionalOnProperty(value = "spring.cloud.nacos.discovery.enabled", matchIfMissing = true)
+    public NacosDiscoveryProperties nacosProperties() {
+        NacosDiscoveryProperties nacosDiscoveryProperties = new NacosDiscoveryProperties();
+        Map<String, String> metadata = nacosDiscoveryProperties.getMetadata();
+        metadata.put("env", StringUtils.join(env.getActiveProfiles()));
+        metadata.put("version", env.getProperty(APP_VERSION));
+        metadata.put("startup.time", OffsetDateTime.now().format(DateTimeFormatter.ISO_OFFSET_DATE_TIME));
+        return nacosDiscoveryProperties;
+    }
+}
